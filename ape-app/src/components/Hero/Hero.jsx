f802b1dab5f16d4dac71a3c05cdf58db4b33bbba
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-19 14:54:31 +0200

    hero_ mobile? add fontface

--- ape-app/src/components/Hero/Hero.jsx
+++ ape-app/src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import css from "./Hero.module.css";
-import ButtonMeetApes from "./ButtonMeetApes";
+
 import apeImageMobile1x from "../../images/mobile/HeroApe-mob1x-min.png";
 import apeImageMobile2x from "../../images/mobile/HeroApe-mob2x-min.png";
 import apeImageTablet1x from "../../images/tablet/HeroApe-tab1x-min.png";
@@ -14,33 +14,32 @@ const Hero = () => {
       <section className={css.heroSection}>
         <p className={css.heroPreTitle}>diD yOu seE iT ?</p>
         <h1 className={css.heroMainTitle}>YACHT APES</h1>
-        <p className={css.heroPreTitle}>Apes aRe eveRywhere</p>
+        <p className={css.heroPostTitle}>Apes aRe eveRywhere</p>
         <picture>
           <source
             srcSet={`${apeImageDesk1x} 1x, ${apeImageDesk2x} 2x`}
             media="(min-width: 1280px)"
-            sizes="min-width: 150px"
           />
 
           <source
             srcSet={`${apeImageTablet1x} 1x, ${apeImageTablet2x} 2x`}
             media="(min-width: 768px)"
-            sizes="min-width: 150px"
           />
           <source
             srcSet={`${apeImageMobile1x} 1x, ${apeImageMobile2x} 2x`}
             media="(max-width: 480px)"
-            sizes="150px"
           />
           <img
-            className={css.gooseImage}
+            className={css.apeImage}
             src={apeImageMobile1x}
             alt="Ape"
             loading="lazy"
           />
         </picture>
-        <ButtonMeetApes />
-        <p>
+        <button className={css.buttonMeet} type="button">
+          Meet Apes
+        </button>
+        <p className={css.heroDescr}>
           Yacht Ape is a collection of unique digital apes that you can own in
           NFT format
         </p>
