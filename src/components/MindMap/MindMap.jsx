864ef8a737ca8b8242b5671f5d7bc43b5681b1f5
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-20 00:39:56 +0200

    json

--- src/components/MindMap/MindMap.jsx
+++ src/components/MindMap/MindMap.jsx
@@ -1,2 +1,11 @@
 import React from "react";
 import css from "./MindMap.module.css";
+
+conat MindMap = () => {
+    return (
+        <section className={css.mindMap}>
+            <h1>Mind Map</h1>
+            
+        </section>
+    )
+}
