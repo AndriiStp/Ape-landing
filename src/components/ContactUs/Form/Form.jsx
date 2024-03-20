bb2b3097b2820627ada7b8224686ddc2edaca408
Andrii Stepanenko <andrii.stp@gmail.com>
2024-03-20 16:22:27 +0200

    input Fields add icons

--- src/components/ContactUs/Form/Form.jsx
+++ src/components/ContactUs/Form/Form.jsx
@@ -1,8 +1,10 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { object, string, number, date, InferType } from "yup";
+import * as yup from "yup";
 import React from "react";
 import css from "./Form.module.css";
 
+import svgDiscord from "../../../images/svg/discord.svg";
+import svgMetamask from "../../../images/svg/SVG_MetaMask_Icon_Color 1.svg";
 const ContactForm = () => {
   const handleSubmit = (values, { resetForm }) => {
     console.log(values);
@@ -14,27 +16,52 @@ const ContactForm = () => {
     metamask: "",
   };
 
-  let userSchema = object({
-    discord: string().required(),
-    metamask: number().required().positive().integer(),
+  let userSchema = yup.object({
+    discord: yup.string().min(6).max(16).required("Wrong discord"),
+    metamask: yup.string().min(6).max(16).required("Wrong ADDRESS"),
   });
 
   return (
     <>
-      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+      <Formik
+        onSubmit={handleSubmit}
+        validationSchema={userSchema}
+        initialValues={initialValues}
+      >
         <Form autoComplete="off" className={css.contactForm}>
-          <Field
-            placeholder="@username"
-            className={css.contactField}
-            type="text"
+          <div className={css.inputWapper}>
+            <div className={css.inputBefore}>
+              <img src={svgDiscord} alt="" />
+            </div>
+            <Field
+              placeholder="@username"
+              className={css.contactField}
+              type="text"
+              name="discord"
+            ></Field>
+          </div>
+          <ErrorMessage
+            className={css.contactError}
             name="discord"
-          ></Field>
-          <Field
-            placeholder="Wallet address"
-            className={css.contactField}
-            type="text"
+            component="div"
+          />
+          <div className={css.inputWapper}>
+            <div className={css.inputBefore}>
+              <img src={svgMetamask} alt="" />
+            </div>
+            <Field
+              placeholder="Wallet address"
+              className={css.contactField}
+              type="text"
+              name="metamask"
+            ></Field>
+          </div>
+          <ErrorMessage
+            className={css.contactError}
             name="metamask"
-          ></Field>
+            component="div"
+          />
+
           <button className={css.contactSubmit} type="submit">
             Mint
           </button>
