b47bd55aaac9c04d1dabe27da0697aa59087aebf
Andrii Stepanenko <andrii.stp@gmail.com>
2024-04-09 15:17:30 +0300

    recomponent swiper controller

--- src/components/Arts/CollectionSlider/CollectionSlider.jsx
+++ src/components/Arts/CollectionSlider/CollectionSlider.jsx
@@ -1,10 +1,10 @@
-// import { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import css from './CollectionSlider.module.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { apesItems } from '../../../style/const/apeCollection';
+import SwiperController from '../../SwiperController/SwiperController';
 
 const CollectionSlider = ({ apes }) => {
   const swiperParams = {
@@ -25,44 +25,35 @@ const CollectionSlider = ({ apes }) => {
   };
 
   return (
-    <>
-      <Swiper className={css.swiperController} {...swiperParams}>
-        {apesItems.map(
-          ({ alt, apeMob1x, apeMob2x, apeBig1x, apeBig2x }, index) => (
-            <SwiperSlide className={css.slideItem} key={index}>
-              <picture>
-                <source
-                  srcSet={`${apeBig1x} 1x, ${apeBig2x} 2x`}
-                  media="(min-width: 1280px)"
-                />
-                <source
-                  srcSet={`${apeBig1x} 1x, ${apeBig2x} 2x`}
-                  media="(min-width: 768px)"
-                />
-                <source
-                  srcSet={`${apeMob1x} 1x, ${apeMob2x} 2x`}
-                  media="(max-width: 480px)"
-                />
-                <img
-                  className={css.imageSizing}
-                  src={apeMob1x}
-                  alt={alt}
-                  loading="lazy"
-                />
-              </picture>
-            </SwiperSlide>
-          )
-        )}
-        <div className={css.sliderNavigation}>
-          <div className="prev">
-            <span className={css.sliderPrev}>Prev</span>
-          </div>
-          <div className="next">
-            <span className={css.sliderNext}>Next</span>
-          </div>
-        </div>
-      </Swiper>
-    </>
+    <Swiper className={css.swiperModule} {...swiperParams}>
+      {apesItems.map(
+        ({ alt, apeMob1x, apeMob2x, apeBig1x, apeBig2x }, index) => (
+          <SwiperSlide className={css.slideItem} key={index}>
+            <picture>
+              <source
+                srcSet={`${apeBig1x} 1x, ${apeBig2x} 2x`}
+                media="(min-width: 1280px)"
+              />
+              <source
+                srcSet={`${apeBig1x} 1x, ${apeBig2x} 2x`}
+                media="(min-width: 768px)"
+              />
+              <source
+                srcSet={`${apeMob1x} 1x, ${apeMob2x} 2x`}
+                media="(max-width: 480px)"
+              />
+              <img
+                className={css.imageSizing}
+                src={apeMob1x}
+                alt={alt}
+                loading="lazy"
+              />
+            </picture>
+          </SwiperSlide>
+        )
+      )}
+      <SwiperController />
+    </Swiper>
   );
 };
 
